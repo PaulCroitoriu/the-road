fd0039d9fe1878de5eb178920dd5050884be85c2
PaulCroitoriu <croitoriu.pavel@gmail.com>
2020-11-16 20:05:51 +0000

    client cache

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      result: null,
+      results: null,
+      searchKey: "",
       searchTerm: DEFAULT_QUERY,
     };
   }
@@ -27,12 +28,25 @@ class App extends React.Component {
   setSearchTopStories = result => {
     // destructuring hits and page from the result state
     const { hits, page } = result;
+    const { searchKey, results } = this.state;
 
-    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
 
     const updatedHits = [...oldHits, ...hits];
 
-    this.setState({ result: { hits: updatedHits, page } });
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page },
+      },
+    });
+  };
+
+  //searching for cache results
+  needsToSearchTopStories = searchTerm => {
+    //takes the serchTerm as input and returns if there are no results based on searchTerm key
+    return !this.state.results[searchTerm];
   };
 
   // method to fetch data from api
@@ -46,23 +60,39 @@ class App extends React.Component {
   };
 
   componentDidMount() {
+    const { searchTerm } = this.state;
+
+    //update searchKey
+    this.setState({ searchKey: searchTerm });
     // update state
-    this.fetchSearchTopStories(this.state.searchTerm);
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onDismiss = id => {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
     //check for the item id and filter the list
-    const updatedHits = this.state.result.hits.filter(
-      item => item.objectID !== id
-    );
+    const updatedHits = hits.filter(item => item.objectID !== id);
+
     //update state with the updated list
     this.setState({
-      result: { ...this.state.result, hits: updatedHits },
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page },
+      },
     });
   };
 
   onSearchSubmit = event => {
-    this.fetchSearchTopStories(this.state.searchTerm);
+    const { searchTerm } = this.state;
+    //update searchKey
+    this.setState({ searchKey: searchTerm });
+
+    //checks if there are no results[searchTherm]
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
     event.preventDefault();
   };
 
@@ -72,11 +102,16 @@ class App extends React.Component {
   };
 
   render() {
-    const { result, searchTerm } = this.state;
+    const { results, searchTerm, searchKey } = this.state;
 
-    const page = (result && result.page) || 0;
+    const page =
+      (results && results[searchKey] && results[searchKey].page) || 0;
 
-    console.log(this.state);
+    const list =
+      (results && results[searchKey] && results[searchKey].hits) || [];
+
+    console.log(this.state.results);
+
     return (
       <div className="page">
         <div className="interactions">
@@ -88,13 +123,11 @@ class App extends React.Component {
             <h1>Search</h1>
           </Search>
         </div>
-        {result ? (
-          <Table list={result.hits} onDismiss={this.onDismiss} />
-        ) : null}
+        <Table list={list} onDismiss={this.onDismiss} />
         <div className="interactions">
           <Button
             candApesiClick={() =>
-              this.fetchSearchTopStories(searchTerm, page + 1)
+              this.fetchSearchTopStories(searchKey, page + 1)
             }
           >
             More
